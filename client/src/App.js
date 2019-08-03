fc82e733df557af7d0e0c51599efb78ad3b06f13
araltiparmak <aliriza_altiparmak@hotmail.com>
2019-08-03 17:40:25 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Fib from './Fib';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import OtherPage from './OtherPage';
+
+
+function App() {
+  return (
+    <Router>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
